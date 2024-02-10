2d33fb37b81226cdeb795fe94fe63d7418fbd5db
muthomi254 <dmunene202020@gmail.com>
2024-02-10 22:16:01 +0300

    ft(routes): creating(login,profile,logout,landingpage) routes and linking them

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,11 @@
-// // src/App.jsx
-// import React from 'react';
-// // import Register from './components/Register';
-// import Login from './components/Login';
-// // import Logout from './components/Logout';
-// import { AuthProvider } from './context/AuthContext';
-
-// function App() {
-//   return (
-//     <AuthProvider>
-//       {' '}
-//       {/* Wrap the app with AuthProvider */}
-//       <div >
-
-//         {/* <Register /> */}
-//         <Login />
-//         {/* <Logout />  */}
-//       </div>
-//     </AuthProvider>
-//   );
-// }
-
-// export default App;
-
-// import React from 'react';
-// import Login from './components/Login'; // Import the Login component
-// import { AuthProvider } from './context/AuthContext';
-
-// function App() {
-//   return (
-//     <AuthProvider>
-//       {/* Wrap the app with AuthProvider */}
-//       <div>
-//         {/* Render the Login component */}
-//         <Login />
-//       </div>
-//     </AuthProvider>
-//   );
-// }
-
-// export default App;
-
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
 import Layout from './layout/Layout';
 import LandingPage from './pages/LandingPage';
 import LoginPage from './components/Login';
 import RegisterPage from './components/Register';
+import ProfilePage from './components/Profile';
+import Logout from './components/Logout'; // Import the Logout component
 
 function App() {
   return (
@@ -56,6 +16,9 @@ function App() {
             <Route path="/" element={<LandingPage />} />
             <Route path="/login" element={<LoginPage />} />
             <Route path="/register" element={<RegisterPage />} />
+            <Route path="/profile" element={<ProfilePage />} />
+            <Route path="/logout" element={<Logout />} />
+           
             {/* Add more routes here for other pages */}
           </Routes>
         </Layout>
