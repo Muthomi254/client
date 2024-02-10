971a9ecefc065c41bcd6412782dda494d1b46cda
muthomi254 <dmunene202020@gmail.com>
2024-02-10 22:29:56 +0300

    ft(authentication): adding logic to support the auth components(login, profile, logout and register) Preventing an authorised access to profile Ensuring token is recieved and revoked

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,3 +1,60 @@
+// import React, { createContext, useContext, useState } from 'react';
+
+// const AuthContext = createContext();
+
+// export const AuthProvider = ({ children }) => {
+//   const [authToken, setAuthToken] = useState(null);
+
+//   const login = (token) => {
+//     setAuthToken(token);
+//   };
+
+//   const logout = () => {
+//     setAuthToken(null);
+//   };
+
+//   const isAuthenticated = () => !!authToken;
+
+//   const authFetch = async (url, method = 'GET', data = null, headers = {}) => {
+//     const requestOptions = {
+//       method,
+//       headers: {
+//         'Content-Type': 'application/json',
+//         Authorization: authToken ? `Bearer ${authToken}` : '',
+//         ...headers,
+//       },
+//       body: data ? JSON.stringify(data) : undefined,
+//     };
+
+//     const response = await fetch(url, requestOptions);
+
+//     if (!response.ok) {
+//       const errorData = await response.json();
+//       throw new Error(errorData.message);
+//     }
+
+//     return response.json();
+//   };
+
+//   const value = {
+//     login,
+//     logout,
+//     isAuthenticated,
+//     authFetch,
+//   };
+
+//   return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+// };
+
+// export const useAuth = () => {
+//   const context = useContext(AuthContext);
+//   if (!context) {
+//     throw new Error('useAuth must be used within an AuthProvider');
+//   }
+//   return context;
+// };
+
+
 import React, { createContext, useContext, useState } from 'react';
 
 const AuthContext = createContext();
@@ -15,7 +72,11 @@ export const AuthProvider = ({ children }) => {
 
   const isAuthenticated = () => !!authToken;
 
+  // Include the database URL in the authFetch function
   const authFetch = async (url, method = 'GET', data = null, headers = {}) => {
+    // Append the database URL to the provided URL
+    const apiUrl = `http://127.0.0.1:5000${url}`;
+
     const requestOptions = {
       method,
       headers: {
@@ -26,7 +87,7 @@ export const AuthProvider = ({ children }) => {
       body: data ? JSON.stringify(data) : undefined,
     };
 
-    const response = await fetch(url, requestOptions);
+    const response = await fetch(apiUrl, requestOptions);
 
     if (!response.ok) {
       const errorData = await response.json();
